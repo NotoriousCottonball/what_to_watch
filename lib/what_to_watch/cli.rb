dcdcf6514c1a4935f895378e8f25e3d1c81460e9
George  Pianka <gpianka1@gmail.com>
2018-09-01 05:30:48 +0000

    CLI Method Names and Structure

--- lib/what_to_watch/cli.rb
+++ lib/what_to_watch/cli.rb
@@ -1,6 +1,8 @@
 class WhatToWatch::CLI 
+  attr_accessor :input
   
-  def start 
+  def initialize 
+   @input = nil
    puts "=======================================================================" 
    puts %q[
  _  _  _  _   __  ____      ____  __        _  _   __  ____  ___  _  _ 
@@ -13,6 +15,34 @@ class WhatToWatch::CLI
    puts ""
    puts ""
    puts "Welcome to What To Watch!"
-   
+   puts ""
   end 
+   
+  def start
+   while input != "exit"
+  end 
+  
+  
+  #CLI Dialogue Methods
+  def menu 
+  end
+  
+  def options
+  end
+  
+  def invalid_command 
+  end
+  
+  def exit 
+    puts "See You Next Time for More Great Movie/TV Recommendations!"
+  end
+  
+  #CLI Logic Methods
+  def exit?
+    @input.downcase == "exit"
+  end
+  
+  def valid_number?(array)
+    @input.to_i.between?(1, array.size)
+  end
 end
