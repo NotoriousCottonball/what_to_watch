536b5d5303f1a5b03c3a249b392a4e071c19f4ef
George  Pianka <gpianka1@gmail.com>
2018-09-01 03:37:23 +0000

    what_to_watch.rb file dependencies and gem dependencies

--- lib/what_to_watch.rb
+++ lib/what_to_watch.rb
@@ -1,5 +1,17 @@
-require "what_to_watch/version"
+require "open-uri"
+require "nokogiri"
+require "pry"
 
-module WhatToWatch
-  # Your code goes here...
-end
+
+# File Dependencies.
+#Load version.rb first 
+#to define Module WhatToWatch.
+
+require "what_to_watch/version"
+require "what_to_watch/cli"
+require "what_to_watch/scraper"
+require "what_to_watch/show"
+require "what_to_watch/best_movies"
+require "what_to_watch/best_tv"
+require "what_to_watch/recently_added"
+
